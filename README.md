180402b9ed129cd7e26ea9247aa91a0936aea157
Mustafa <971530+tosbaha@users.noreply.github.com>
2022-09-08 13:42:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Appcircle Wait for Android emulator component
+
+Wait for Android emulator to boot
+
+## Required Inputs
+- `AC_TEST_DEVICE`: Test Device. Specifies device for test. If you use emulator other than Pixel_3a, you need to create it by yourself.
+- `AC_TEST_ADB_ARGUMENTS`: ADB arguments for the device. You may add new arguments but don't change the default ones such as no-window.
+
+## Optional Inputs
+- `AC_TEST_ADB_WAIT_SECONDS`: Boot Timeout. Specifies the number of seconds the component must wait for the emulator to boot.
---
+++ main.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+require 'English'
+require 'pathname'
+require 'timeout'
+
+def get_env_variable(key)
+  ENV[key].nil? || ENV[key] == '' ? nil : ENV[key]
+end
+
+def run_command(cmd)
+  puts "@@[command] #{cmd}"
+  output = `#{cmd}`
+  raise "Failed to execute - #{cmd}" unless $CHILD_STATUS.success?
+
+  output
+end
+
+def list_devices
+  output = run_command('adb devices').split("\n")
+  return [] unless output.count > 1
+
+  output.drop(1).map { |line| line.split("\t").first }
+end
+
+def installed_avd?(name)
+  emulators = run_command("#{emulator_path} -list-avds").split("\n")
+  emulators.include? name
+end
+
+def adb_prop_eq?(prop, expected)
+  cmd = "adb shell getprop #{prop}"
+  ret = `#{cmd} 2>&1`.strip
+  puts "getprop #{prop} : '#{ret}'"
+  ret == expected
+end
+
+def emulator_ready?
+  adb_prop_eq?('dev.bootcomplete',       '1')       or return false
+  adb_prop_eq?('sys.boot_completed',     '1')       or return false
+
+  (adb_prop_eq?('service.bootanim.exit', '1') ||
+      adb_prop_eq?('init.svc.bootanim', 'stopped')) or return false
+  true
+end
+
+def emulator_path
+  android_home = get_env_variable('ANDROID_HOME') || abort('Missing ANDROID_HOME variable.')
+  Pathname.new(android_home).join('emulator/emulator').to_s
+end
+
+def wait_emulator
+  attempt = 1
+  cool_down = 10
+  loop do
+    puts "[I] Attempt ##{attempt}"
+    break if emulator_ready?
+
+    puts "[I] Waiting #{cool_down} seconds before next attempt"
+    attempt += 1
+    sleep cool_down
+  end
+end
+
+def wait_for_emulator(name, duration)
+  begin
+    Timeout.timeout(duration) do
+      wait_emulator
+    end
+  rescue Timeout::Error
+    puts "(X) Starting #{name} timed out"
+    return false
+  end
+  true
+end
+
+def start_emulator(name, arguments)
+  run_command('adb start-server')
+  cmd = "#{emulator_path} -avd #{name} #{arguments} &"
+  result = system(cmd)
+  puts result
+  sleep 5
+end
+
+def start_and_wait_for_emulator(name, arguments, duration)
+  unless installed_avd?(name)
+    puts "(X) Emulator '#{name}' not found"
+    return false
+  end
+  start_emulator(name, arguments)
+  status = wait_for_emulator(name, duration)
+  puts "[I] Emulator #{name} is ready" if status == true
+  status
+end
+
+emulator_name = get_env_variable('AC_TEST_DEVICE') || 'Pixel_3a'
+wait_period = get_env_variable('AC_TEST_ADB_WAIT_SECONDS') || '300'
+arguments = get_env_variable('AC_TEST_ADB_ARGUMENTS')
+
+status = start_and_wait_for_emulator(emulator_name, arguments, wait_period.to_i)
+if status == true
+  puts "[I] Running emulators #{list_devices}"
+else
+  exit(1)
+end
